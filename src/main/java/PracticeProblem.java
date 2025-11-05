56d55b0b842980109bea2d3f5b79ea1781b01d94
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-11-05 13:21:41 -0500

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,66 @@
+import java.util.Arrays;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 		
 	}
+//Question 1
+public static int[] compareSearch(int[] array, int number) {
+//Sequential Search
+	int seqCount = 0;
+	for (int i = 0; i < array.length; i++) {
+		seqCount++;
+		if (array[i] == number) {
+			break;
+		}
+	}
+	Arrays.sort(array);
+	int low = 0;
+	int binCount = 0;
+	int high = array.length - 1;
+	while (low <= high) {
+		binCount++;
+		int middlePosition = (low + high) / 2;
+		int middleNumber = array[middlePosition];
+		if (number == middleNumber) {
+			break; }
+			if (number < middleNumber) {
+				high = middlePosition - 1;
+			}
+			else {
+				low = middlePosition + 1;
+			}
+			}
+		return new int[] {seqCount, binCount};
+	}
+	//Question 2
+	public static int[] compareStringSearch(String[] array, String word) {
+	//Sequential Search
+		int seqCount = 0;
+		for (int i = 0; i < array.length; i++) {
+			seqCount++;
+			if (array[i].equalsIgnoreCase(word)) {
+				break;
+			}
+		}
+		Arrays.sort(array);
+		int low = 0;
+		int binCount = 0;
+		int high = array.length - 1;
 	
-}
+		while (low <= high) {
+			binCount++;
+			int middlePosition = (low + high) / 2;
+			int middleNumber = array[middlePosition].compareTo(word);
+			if (middleNumber == 0) {
+				break; }
+				if (middleNumber < 0) {
+					low = middlePosition + 1;
+				}
+				else {
+					high = middlePosition - 1;
+				}
+				}
+			return new int[] {seqCount, binCount};
+		}
+	}
